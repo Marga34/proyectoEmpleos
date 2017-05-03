07b86a6bab7131f2263cf6e0a71f8d87853cb60f
Marga34 <mmartindearce@hotmail.com>
2017-05-03 13:12:00 +0200

    he anadido 3 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './lista/lista.component';
+import { FormComponent } from './form/form.component';
+import { DescrComponent } from './descr/descr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    FormComponent,
+    DescrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/descr/descr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descr',
+  templateUrl: './descr.component.html',
+  styleUrls: ['./descr.component.css']
+})
+export class DescrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
