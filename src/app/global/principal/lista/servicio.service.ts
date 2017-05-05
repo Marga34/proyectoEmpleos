570675db0a35edf55b92e2254946dcf194b2e148
Alejandra <alejandrita1900@gmail.com>
2017-05-05 10:13:09 +0200

    arreglado el footer

--- src/app/global/principal/lista/servicio.service.ts
+++ src/app/global/principal/lista/servicio.service.ts
@@ -4,7 +4,7 @@ import { Oferta } from './oferta';
 @Injectable()
 export class ServicioService implements OnInit {
 
-private ofertas: Array<Oferta> = [new Oferta (1, 'Mercadona', 'Programador', 'Almeria', 'Sin Experiencia', 'Temporal'),new Oferta (1, 'Mercadona', 'Programador', 'Almeria', 'Sin Experiencia', 'Temporal')];
+private ofertas: Array<Oferta> = [new Oferta (1, 'Mercadona', 'Programador', 'Almeria', 'Sin Experiencia', 'Temporal'),new Oferta (1, 'Samsung', 'Big Data', 'Madrid', 'Con Experiencia', 'Indefinido')];
 
 constructor() { }
 
